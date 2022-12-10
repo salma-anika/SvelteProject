4a6c897722010af34b5a8c0a214e1ada0684801a
salma-anika <anikaavaa@gmail.com>
2022-12-11 03:34:06 +0600

    design and responsive  contact section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -125,7 +125,48 @@
 <div >
     
 </div>
+<div id="contact" class="w-full h-screen">
+	<div class="max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left">
+		<h2
+			class="text-7xl text-center md:text-3xl uppercase tracking-wider font-bold top-2 text-sky-700"
+		>
+			Contact
+		</h2>
 
+		<p class="project py-4 text-center">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+		</p>
+    <div class='w-full md:w-full h-full rounded-xl p-4'>
+        <div class='p-4 text-left'>
+            <form action='' id="contactForm">
+                <div class='grid md:grid-cols-2 gap-4 w-full py-4 sm:grid-cols-1'>
+                    <div class='flex flex-col '>
+                    <label class='capitalize text-sm py-2 text-slate-600 font-light'>name</label>
+                    <input type="text" id="" name="contactname"  class='border-2 rounded-lg p-3 flex focus:outline-none border-blue-200'/>
+                </div>
+                <div class='flex flex-col'>
+                    <label class='capitalize text-sm py-2 text-slate-600 font-light'>phone</label>
+                    <input type="text" id="" name="phone" class='border-2 rounded-lg p-3 flex focus:outline-none border-blue-200'/>
+                </div>
+                <div class='flex flex-col col-span-2 '>
+                    <label class='capitalize text-sm py-2 text-slate-600 font-light'>email</label>
+                    <input type="email" id="" name="email"  class='border-2 rounded-lg p-3 flex focus:outline-none border-blue-200'/>
+                </div>
+                <div class='flex flex-col col-span-2 '>
+                    <label class='capitalize text-sm py-1 text-slate-600 font-light'>message</label>
+                    <textarea  id="" rows="10" name="message"  class='border-2 rounded-lg  flex focus:outline-none border-blue-200'/>
+                </div>
+                <div class=''>
+       
+                    <a href="#" id="submit">Submit</a>
+
+        </div>
+               </div>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
 <style>
 	#header {
 		position: absolute;
@@ -220,6 +261,20 @@
 		transform: translateY(100px);
 		margin-top: 130px;
 	}
+
+    #submit{
+        text-decoration: none;
+		display: inline-block;
+		padding: 8px 30px;
+		background: #094b65;
+		color: #fff;
+		font-size: 1.2em;
+		font-weight: 500;
+		letter-spacing: 2px;
+		border-radius: 40px;
+		transform: translateY(100px);
+		
+    }
 	#btn:hover {
 		background: #094b65;
 		color: #fff;
