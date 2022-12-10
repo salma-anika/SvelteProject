5597771ae39ff08a3d1cfa86de1493f51472ffb9
salma-anika <anikaavaa@gmail.com>
2022-12-11 03:09:53 +0600

    design project section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -61,7 +61,26 @@
     
 </div>
 
-
+<div id="laravel" class="w-full h-screen">
+    <div class="max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left">
+       <h2 class="text-5xl text-center md:text-7xl uppercase tracking-wider font-bold top-2 text-sky-600"> Project</h2>
+      
+                <p class="project py-4 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+  do eiusmod tempor 
+                </p>
+                <div class='mx-auto max-w-5xl  grid md:grid-cols-3 gap-8'>
+               
+                    <div class='cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md'>
+<img class="rounded-md duration-200 hover:scale-110" src='/background.jpg' width="350" height="350" alt=""/>
+<h2 class='text-center text-base capitalize my-4 font-light duration-200 group-hover:underline underline-offset-4'>Web Project</h2>
+                    </div>
+               
+                
+      
+            </div>
+                </div>
+                </div>
 
 <style>
     #header{
