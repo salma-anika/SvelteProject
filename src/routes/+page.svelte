acfaece2d87a9f6d7ed251738c9a10b6706a7a0b
salma-anika <anikaavaa@gmail.com>
2022-12-10 20:27:26 +0600

    text center in about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,10 +44,7 @@
          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-         and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-         and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        </p>
 </div>
 
 <div class="project">
@@ -167,6 +164,7 @@ h2#text span{
     color: #f5f5f5;
     font-weight: 600;
     font-family: 'Noto Serif', serif;
+    text-align: center;
 }
 .sec p{
     font-size: 16px;
